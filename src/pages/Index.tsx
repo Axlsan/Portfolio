8f959ecdbf20db11f4ce0325600509558895f42f
Axlsan <axel_sanne@hotmail.com>
2026-06-25 23:48:59 +0200

    gap upd

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -150,7 +150,7 @@ const Index = () => {
               <Stat k="Coffee/day" v="∞" />
             </div>
             */}
-            <div className="grid md:grid-cols-2 gap-3 mt-8">
+            <div className="grid md:grid-cols-2 gap-2 mt-8">
               <div>
                 <div className="hud-label mb-3">Softwares</div>
                 <ul className="mono-font text-sm space-y-1.5 text-foreground/80">
